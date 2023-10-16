6165981bb7316e422b199926e5c12d89030c89cc
Sherazi12 <80716930+Sherazi12@users.noreply.github.com>
2023-10-16 18:27:09 +0500

    Home
    Home commit 60 percent

--- src/Doker/Home.js
+++ src/Doker/Home.js
@@ -575,7 +575,7 @@ export const Home = ({ geNetworkData, Networklist, CreateNetwork, ImportImg, con
                           <p className='text-teal-400 text-sm'> alpine/git</p>
                         </div>
                       </div>
-                      <div className='flex justify-evenly px-8 items-center w-full'>
+                      <div style={{marginTop:'5%'}} className='flex justify-evenly px-8 items-center w-full'>
                         <svg onClick={() => setRunPopupVisible({ isopen: true, id: "card_" + index })} className="cursor-pointer" width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M10.2154 18.9045C9.99558 19.0443 9.80607 19.0443 9.64688 18.9045C9.49527 18.7519 9.38915 18.5359 9.3285 18.2563C9.26786 17.9639 9.26786 17.6779 9.3285 17.3983C9.38915 17.106 9.52559 16.8899 9.73785 16.7501L13.2514 14.4814L9.73785 12.2317C9.52559 12.0919 9.38915 11.8758 9.3285 11.5835C9.26786 11.2911 9.26786 11.0051 9.3285 10.7255C9.38915 10.4332 9.49527 10.2108 9.64688 10.0582C9.80607 9.90572 9.99558 9.90572 10.2154 10.0582L14.4112 12.8227C14.8812 13.1277 15.4615 13.6933 15.4615 14.4814C15.4615 15.2948 14.8812 15.835 14.4112 16.14L10.2154 18.9045Z" fill="#C89125" />
                           <path d="M17.7 19.8895C17.5256 19.8895 17.3816 19.7942 17.2679 19.6035C17.1618 19.4256 17.1087 19.2032 17.1087 18.9362C17.1087 18.6566 17.1618 18.4215 17.2679 18.2308C17.3816 18.0529 17.5256 17.9639 17.7 17.9639H21.964C22.1308 17.9639 22.2672 18.0529 22.3733 18.2308C22.487 18.4215 22.5439 18.6566 22.5439 18.9362C22.5439 19.2032 22.487 19.4256 22.3733 19.6035C22.2672 19.7942 22.1308 19.8895 21.964 19.8895H17.7Z" fill="#C89125" />
@@ -613,7 +613,7 @@ export const Home = ({ geNetworkData, Networklist, CreateNetwork, ImportImg, con
 
             {/*Run popup */}
             {runPopupVisible.isopen && (
-              <div className="popup w-2/6 h-2/3">
+              <div className="popup w-2/6 h-4/5">
                 <>
                   <svg className='ml-3' width="41" height="34" viewBox="0 0 41 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M19.5405 0.196731C20.1534 -0.0655785 20.8466 -0.0655768 21.4595 0.196735C24.5565 1.52222 27.6536 2.80552 30.7507 4.08882C33.6765 5.30113 36.6023 6.51345 39.528 7.76134C40.4218 8.14256 41 9.02386 41 9.99822V24.0018C41 24.9761 40.4218 25.8574 39.528 26.2387C36.6019 27.4868 33.6756 28.6993 30.7493 29.9118C27.6526 31.1949 24.556 32.478 21.4595 33.8033C20.8466 34.0656 20.1534 34.0656 19.5405 33.8033C16.4435 32.4778 13.3464 31.1945 10.2492 29.9111C7.32343 28.6988 4.39764 27.4865 1.47196 26.2387C0.578183 25.8574 0 24.9761 0 24.0018V9.99821C0 9.02385 0.578178 8.14256 1.47195 7.76134C4.39814 6.51323 7.32444 5.30071 10.2508 4.08818C13.3474 2.80509 16.444 1.522 19.5405 0.196731ZM36.4445 12.1841C36.4445 11.5132 36.9883 10.9693 37.6593 10.9693C38.3302 10.9693 38.8741 11.5132 38.8741 12.1841V23.4658C38.8741 24.1367 38.3302 24.6806 37.6593 24.6806C36.9883 24.6806 36.4445 24.1367 36.4445 23.4658V12.1841ZM33.1037 12.7974C32.4328 12.7974 31.8889 13.3413 31.8889 14.0122V25.2939C31.8889 25.9648 32.4328 26.5087 33.1037 26.5087C33.7747 26.5087 34.3185 25.9648 34.3185 25.2939V14.0122C34.3185 13.3413 33.7747 12.7974 33.1037 12.7974ZM27.3333 15.8404C27.3333 15.1695 27.8772 14.6256 28.5481 14.6256C29.2191 14.6256 29.7629 15.1695 29.7629 15.8404V27.1221C29.7629 27.793 29.2191 28.3369 28.5481 28.3369C27.8772 28.3369 27.3333 27.793 27.3333 27.1221V15.8404ZM22.4741 17.6685C22.4741 16.9976 23.018 16.4537 23.6889 16.4537C24.3598 16.4537 24.9037 16.9976 24.9037 17.6685V28.9502C24.9037 29.6211 24.3598 30.165 23.6889 30.165C23.018 30.165 22.4741 29.6211 22.4741 28.9502V17.6685Z" fill="#76CDB2" />
@@ -624,7 +624,7 @@ export const Home = ({ geNetworkData, Networklist, CreateNetwork, ImportImg, con
                     <p className='text-teal-400 text-sm'> alpine/git</p>
                   </div>
                 </>
-                <div className=' '>
+                <div style={{marginBottom:'8%'}} className=' '>
                   <button onClick={() => setOptionalData(!optionalData)} className="w-5/6 text-white hover:bg-teal-400  font-bold py-2 px-4 rounded  justify-between flex items-center">
                     <span className='mr-3'>Optional Data</span>
                     {optionalData ?
@@ -699,7 +699,7 @@ export const Home = ({ geNetworkData, Networklist, CreateNetwork, ImportImg, con
 
                     </div> : ''}
                 </div>
-                <div className='flex justify-around ' style={{bottom:'5%', position:'absolute', alignSelf:'center'}}>
+                <div className='flex justify-around ' style={{bottom:'2%', position:'absolute', alignSelf:'center'}}>
                   <button onClick={() => RunHandler()} className="mt-8 w-20 bg-teal-400  font-bold py-1 px-4 rounded  items-center">
                     Run
                   </button>
@@ -874,7 +874,7 @@ export const Home = ({ geNetworkData, Networklist, CreateNetwork, ImportImg, con
                   type="search"
                   id="default-search"
                   className="block w-11/12 p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search Mockups, Logos..."
+                  placeholder="Search Image"
                   required
                 /> <button type="submit" className="text-white  right-2.5 bottom-2.5 bg-teal-400  hover:bg-teal-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 bg-teal-400  dark:focus:ring-blue-800">Search</button>
               </div>
